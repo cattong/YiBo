e1647b43c4208057ab232edb0fcca6e28b68521b
cattong <cattong@cattong-THINK>
2013-03-07 11:19:08 +0800

    更新

--- YiBo/src/com/shejiaomao/weibo/db/CursorDataExtractor.java
+++ YiBo/src/com/shejiaomao/weibo/db/CursorDataExtractor.java
@@ -6,8 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 /**
  * CursorDataExtractor
  *
- * @author 马庆升
- * @time 2010-8-10 下午04:26:01
+ *
  * @param <T>
  */
 public interface CursorDataExtractor<T> {
