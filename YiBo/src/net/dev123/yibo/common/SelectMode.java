bf1fe241b6f18c55156bbb8b19a59517c7af2e13
cattong <cattong@cattong-THINK>
2013-03-07 11:12:05 +0800

    修改本地文件

--- YiBo/src/net/dev123/yibo/common/SelectMode.java
+++
@@ -1,6 +0,0 @@
-package net.dev123.yibo.common;
-
-public enum SelectMode {
-	Single, //单选
-	Multiple //多选
-}
--- YiBo/src/net/dev123/yibo/service/cache/ReclaimLevel.java
+++
@@ -1,7 +0,0 @@
-package net.dev123.yibo.service.cache;
-
-public enum ReclaimLevel {
-	LIGHT,   //轻级别回收
-	MODERATE, //适度回收;	
-	WEIGHT;  //重级别回收
-}
